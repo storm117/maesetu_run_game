50844259f85b682ff1471320436dc421899d72bc
だいち <63898060+iudaichi@users.noreply.github.com>
2020-11-16 17:25:39 +0900

    iroiro

---
+++ Assets/ScoreText.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;  ////ここを追加////
+
+public class ScoreText : MonoBehaviour {
+    // Use this for initialization
+    void Start () {
+        this.GetComponent<Text>().text = "点数";
+    }
+
+    // Update is called once per frame
+    void Update () {
+        this.GetComponent<Text>().text = "点数";
+    }
+}
---
+++ Assets/URL2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+ 
+public class URL2 : MonoBehaviour
+{
+    public void onClick()
+    {
+        string url_;
+        url_ = String.Format("https://line.me/R/ti/p/@639uwexy");
+        Application.OpenURL(url_);
+    }
+}
